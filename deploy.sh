198d7642b2013384fc1338564eb1a93261a33bdb
Hiram J. Perez <hi+gh@worg.xyz>
2019-03-19 14:21:28 -0600

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Usage ./deploy.sh [tag] [message]
 export NODE_ENV='production'
-email='hi+gh@worg.xyz'
 origin=$(git remote get-url origin) # get current repo remote
 branch='master';
 defaultTag="build_$(date +'%Y%m%dT%H%M%S')";
@@ -11,22 +10,17 @@ echo "Creating Tag: $tag"
 
 # Generate static files
 yarn export;
+cd $outDir || exit 1; 
 # Push to specified branch
-if [ !-d $outDir ]; then 
-  echo "${outDir} not found";
-  exit 1;
-fi
-cp ./CNAME $outDir
-cd $outDir;
-touch .nojekyll # Avoid skippink _next on GH pages 
+cp ../CNAME .
+touch .nojekyll # Avoid skipping _next on GH pages 
 cp 404/index.html 404.html # Custom 404 everywhere
 git init;
-git config user.email $email
-git checkout -b $branch;
-git remote add origin $origin;
+git checkout -b "$branch";
+git remote add origin "$origin";
 git add -A .;
 git commit -m "${2:-$defaultTag};"
-git tag -a $tag -m "${2:-$defaultTag};"
-git push -f -u origin $branch;
+git tag -a "$tag" -m "${2:-$defaultTag};"
+git push -f -u origin "$branch";
 echo 'done!';
 exit 0;
